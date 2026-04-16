4522807cde904484d5a0b6547113747c32436eea
Juampa1907 <juampasemi19@gmail.com>
2026-04-15 22:55:59 -0500

    Inicio de proyecto de Desarrollo de sistemas de información 3 con spring boot y angular, final de semestre

---
+++ src/main/java/co/edu/unbosque/FinalProject/FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectApplication.class, args);
+	}
+
+}
